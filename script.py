bdb67addfb499a21d4844d5970d3550947aa133f
vifatala <vifatala@gmail.com>
2023-09-16 04:01:15 -0400

    Code to convert CSV to Python list completed

--- script.py
+++ script.py
@@ -1,13 +1,16 @@
 import csv
 
-# Importing data from the CSV file.
-with open('songs_database.csv', mode='r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        print(f'\t{row["name"]} works in the {row["department"]} department, and was born in {row["birthday month"]}.')
-        line_count += 1
-    print(f'Processed {line_count} lines.')
+def get_dict(file):
+    # Importing data from the CSV file.
+    with open(file, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        csv_list = []
+        for row in csv_reader:
+            name = row['Song Name']
+            album = row['Album']
+            artist = row['Band Name']
+            genre = row['Genre']
+            csv_list.append(list((name, album, artist, genre)))
+            #print(f"\tThe song {row['Song Name']} is a {row['Genre']} track from the album \'{row['Album']}\' by {row['Band Name']}.") # First test handling data from CSV
+    print(csv_list)
+get_dict('songs_database.csv')

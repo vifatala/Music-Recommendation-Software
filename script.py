a2f621dc478030430d34cdc61dbc7340a0a3cc3a
vifatala <vifatala@gmail.com>
2023-09-16 02:29:20 -0400

    Creating first files and database

---
+++ script.py
@@ -0,0 +1,13 @@
+import csv
+
+# Importing data from the CSV file.
+with open('songs_database.csv', mode='r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        print(f'\t{row["name"]} works in the {row["department"]} department, and was born in {row["birthday month"]}.')
+        line_count += 1
+    print(f'Processed {line_count} lines.')

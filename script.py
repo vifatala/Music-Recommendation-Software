47a6f5d5e8dba3bc156254a216b33c21b6ebddf0
vifatala <vifatala@gmail.com>
2023-09-26 18:18:20 -0400

    Finishing project!

--- script.py
+++ script.py
@@ -21,7 +21,7 @@ def search_area():
             for value in possible:
                 print("-"+value)
         elif goal_path is not None:
-            print("Path found")
+            print("Term found in:")
             for node in goal_path:
                 print(f"{' |'  * goal_path.index(node)}-{node.value}")
             print('')
@@ -39,7 +39,8 @@ def search_area():
                 for child in goal_node.children:
                     print(f"-{child.value}")
             else:
-                print(f"{goal_node.value} is a track of the album \'{goal_node.parent.value}\' released by {goal_node.parent.parent.value}.\nThis is considered part of the {goal_node.parent.parent.parent.value} genre.")
+                print(f"\"{goal_node.value}\" is a track of the album \'{goal_node.parent.value}\' released by {goal_node.parent.parent.value}.\nThis is considered part of the {goal_node.parent.parent.parent.value} genre.\n")
+                print(f"(Try searching for \'{goal_node.parent.parent.parent.value}\' to see more songs like this!)")
         else:
             print(f"Sorry, Victor, we could't find anything that started with \"{user_search}\" in our catalog!")
             try_again()
